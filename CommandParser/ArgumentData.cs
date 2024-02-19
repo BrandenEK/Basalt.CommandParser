94e168a081ed76bd1b9f4a49b4aaf6a428da952b
unknown <brandenek03912@hotmail.com>
2024-02-19 09:33:22 -0500

    core: add arguments class

--- CommandParser/ArgumentData.cs
+++ CommandParser/ArgumentData.cs
@@ -3,8 +3,41 @@ namespace CommandParser;
 
 public abstract class ArgumentData
 {
+    //public ArgumentData()
+    //{
+    //    _validArgs = new();
+    //}
+
+    //public ArgumentData(params Argument[] validArgs)
+    //{
+    //    _validArgs = new(validArgs);
+    //}
+
     public void Evaluate(string[] args)
     {
+    }
 
+    private readonly List<Argument> _validArgs = new();
+
+    public void AddValidArgument(Argument argument) => _validArgs.Add(argument);
+}
+
+public class Argument
+{
+    private readonly string _cmdInitial;
+    private readonly string _cmdFull;
+
+    private readonly Action<string> _result;
+
+    public Argument(string cmdInitial, string cmdFull, Action<string> result)
+    {
+        _cmdInitial = cmdInitial;
+        _cmdFull = cmdFull;
+        _result = result;
+    }
+
+    public bool Accept(string arg)
+    {
+        return false;
     }
 }
--- CommandParser/TestArgument.cs
+++ CommandParser/TestArgument.cs
@@ -3,6 +3,15 @@ namespace CommandParser;
 
 public class TestArgument : ArgumentData
 {
+    public TestArgument() : base()
+    {
+        AddValidArgument(new Argument("n", "name", x => Name = x));
+        AddValidArgument(new Argument("t", "token", x => Token = x));
+        AddValidArgument(new Argument("e", "encrypt", x => UseEncryption = bool.Parse(x)));
+        AddValidArgument(new Argument("aa", "auto-add", x => AutomaticallyAdd = bool.Parse(x)));
+        AddValidArgument(new Argument("sd", "seach-delay", x => SearchDelay = int.Parse(x)));
+    }
+
     public string Name { get; set; } = string.Empty;
 
     public string Token { get; set; } = string.Empty;
