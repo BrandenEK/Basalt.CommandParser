0b71ff194f2ece5df78e6a5836fdda943a2a702f
unknown <brandenek03912@hotmail.com>
2024-02-19 10:07:06 -0500

    core: argument subclasses for better handling

--- CommandParser/ArgumentData.cs
+++ CommandParser/ArgumentData.cs
@@ -3,16 +3,6 @@ namespace CommandParser;
 
 public abstract class ArgumentData
 {
-    //public ArgumentData()
-    //{
-    //    _validArgs = new();
-    //}
-
-    //public ArgumentData(params Argument[] validArgs)
-    //{
-    //    _validArgs = new(validArgs);
-    //}
-
     public void Evaluate(string[] args)
     {
         for (int i = 0; i < args.Length; i++)
@@ -26,7 +16,7 @@ public abstract class ArgumentData
     private bool ProcessArg(string curr, string? next)
     {
         Console.WriteLine(curr);
-        foreach (var command in _validArgs)
+        foreach (var command in _commands)
         {
             int result = command.Accept(curr, next);
 
@@ -39,33 +29,87 @@ public abstract class ArgumentData
         throw new CommandParserException($"Invalid argument: {curr}");
     }
 
-    private readonly List<Argument> _validArgs = new();
+    private readonly List<Command> _commands = new();
 
-    public void AddValidArgument(Argument argument) => _validArgs.Add(argument);
+    public void AddCommand(Command cmd) => _commands.Add(cmd);
 }
 
-public class Argument
+public abstract class Command
 {
-    private readonly string _cmdInitial;
-    private readonly string _cmdFull;
+    private readonly string _initial;
+    private readonly string _name;
 
+    public Command(string initial, string name)
+    {
+        _initial = '-' + initial;
+        _name = '-' + name;
+    }
+
+    internal int Accept(string curr, string? next)
+    {
+        if (curr != _initial && curr != _name)
+            return -1;
+
+        return Process(curr, next) ? 1 : 0;
+    }
+
+    // return whether to skip ( Change this to recognize if the next arg doesnt have a dash use that ) !!!
+    protected abstract bool Process(string curr, string? next);
+}
+
+public class StringCommand : Command
+{
     private readonly Action<string> _result;
 
-    public Argument(string cmdInitial, string cmdFull, Action<string> result)
+    public StringCommand(string initial, string name, Action<string> result) : base(initial, name)
     {
-        _cmdInitial = cmdInitial;
-        _cmdFull = cmdFull;
         _result = result;
     }
 
-    // Return whether to skip an arg
-    // 0 = valid normal, -1 = na, 1 = skip extra
-    public int Accept(string curr, string? next)
+    protected override bool Process(string curr, string? next)
     {
-        if (curr != "-" + _cmdInitial && curr != "-" + _cmdFull)
-            return -1;
+        if (next is null)
+            throw new CommandParserException($"Command {curr} needs a following argument");
 
-        _result(curr);
-        return 0;
+        _result(next);
+        return true;
+    }
+}
+
+public class BoolCommand : Command
+{
+    private readonly Action<bool> _result;
+
+    public BoolCommand(string initial, string name, Action<bool> result) : base(initial, name)
+    {
+        _result = result;
+    }
+
+    protected override bool Process(string curr, string? next)
+    {
+        _result(true);
+        return false;
+    }
+}
+
+public class IntCommand : Command
+{
+    private readonly Action<int> _result;
+
+    public IntCommand(string initial, string name, Action<int> result) : base(initial, name)
+    {
+        _result = result;
+    }
+
+    protected override bool Process(string curr, string? next)
+    {
+        if (next is null)
+            throw new CommandParserException($"Command {curr} needs a following argument");
+
+        if (!int.TryParse(next, out int value))
+            throw new CommandParserException($"Command {curr} needs to be in integer format");
+
+        _result(value);
+        return true;
     }
 }
--- CommandParser/TestArgument.cs
+++ CommandParser/TestArgument.cs
@@ -5,11 +5,11 @@ public class TestArgument : ArgumentData
 {
     public TestArgument() : base()
     {
-        AddValidArgument(new Argument("n", "name", x => Name = x));
-        AddValidArgument(new Argument("t", "token", x => Token = x));
-        AddValidArgument(new Argument("e", "encrypt", x => UseEncryption = true));
-        AddValidArgument(new Argument("aa", "auto-add", x => AutomaticallyAdd = true));
-        AddValidArgument(new Argument("sd", "seach-delay", x => SearchDelay = int.Parse(x)));
+        AddCommand(new StringCommand("n", "name", x => Name = x));
+        AddCommand(new StringCommand("t", "token", x => Token = x));
+        AddCommand(new BoolCommand("e", "encrypt", x => UseEncryption = x));
+        AddCommand(new BoolCommand("aa", "auto-add", x => AutomaticallyAdd = x));
+        AddCommand(new IntCommand("sd", "seach-delay", x => SearchDelay = x));
     }
 
     public string Name { get; set; } = string.Empty;
