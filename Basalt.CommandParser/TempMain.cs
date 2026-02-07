009132918ea22f351970b42d02a1c46ac91f3e21
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-02-06 20:02:25 -0500

    core: temp make executable

---
+++ Basalt.CommandParser/TempMain.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Basalt.CommandParser;
+
+public class TempMain
+{
+    static void Main(string[] args)
+    {
+        CommandParser.Parse<BaseArguments>(args);
+        Console.ReadKey(true);
+    }
+}
