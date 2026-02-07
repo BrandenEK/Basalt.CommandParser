d64fc764cca033f6d4a28f3dd986d1171f8d158e
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-02-07 12:31:12 -0500

    tests: move data types to diff files

--- Basalt.CommandParser.Tests/ProcessingArguments.cs
+++ Basalt.CommandParser.Tests/ProcessingArguments.cs
@@ -5,14 +5,14 @@ namespace Basalt.CommandParser.Tests;
 public class TestArguments : ProgramArguments
 {
     [StringArgument("string", "s", "desc")]
-    public string String { get; set; }
+    public string String { get; set; } = string.Empty;
 
     [BooleanArgument("bool", "b", "desc")]
-    public bool Boolean { get; set; }
+    public bool Boolean { get; set; } = false;
 
     [IntegerArgument("int", "i", "desc")]
-    public int Integer { get; set; }
+    public int Integer { get; set; } = 0;
 
     [FloatArgument("float", "f", "desc")]
-    public float Float { get; set; }
+    public float Float { get; set; } = 0f;
 }
--- Basalt.CommandParser.Tests/ProcessingTests.cs
+++ Basalt.CommandParser.Tests/ProcessingTests.cs
@@ -2,10 +2,9 @@
 
 namespace Basalt.CommandParser.Tests;
 
-[TestClass]
 public class ProcessingTests
 {
-    private void ValidateSuccess(string[] args, Func<TestArguments, bool> isValid)
+    protected void ValidateSuccess(string[] args, Func<TestArguments, bool> isValid)
     {
         var data = new TestArguments();
         data.Process(args);
@@ -13,52 +12,10 @@ public class ProcessingTests
         Assert.IsTrue(isValid(data));
     }
 
-    private void ValidateFailure<TException>(string[] args) where TException : ArgumentProcessingException
+    protected void ValidateFailure<TException>(string[] args) where TException : ArgumentProcessingException
     {
         var data = new TestArguments();
 
         Assert.ThrowsException<TException>(() => data.Process(args));
-    }
-
-    [TestMethod]
-    public void Test_NoArgs()
-    {
-        ValidateSuccess(new string[] { }, data => true);
-    }
-
-    [TestMethod]
-    public void Test_BoolArgs_Nothing()
-    {
-        ValidateSuccess(new string[] { }, data => !data.Boolean);
-    }
-
-    [TestMethod]
-    public void Test_BoolArgs_Short()
-    {
-        ValidateSuccess(new string[] { "-b" }, data => data.Boolean);
-    }
-
-    [TestMethod]
-    public void Test_BoolArgs_Long()
-    {
-        ValidateSuccess(new string[] { "--bool" }, data => data.Boolean);
-    }
-
-    [TestMethod]
-    public void Test_BoolArgs_True()
-    {
-        ValidateSuccess(new string[] { "-b", "true" }, data => data.Boolean);
-    }
-
-    [TestMethod]
-    public void Test_BoolArgs_False()
-    {
-        ValidateSuccess(new string[] { "-b", "FALSE" }, data => !data.Boolean);
-    }
-
-    [TestMethod]
-    public void Test_BoolArgs_Invalid()
-    {
-        ValidateFailure<InvalidParameterException>(new string[] { "-b", "invalid" });
     }
 }
