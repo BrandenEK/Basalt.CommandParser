0fcf14bfd94179a59278f2d41172dd64efcfbcae
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-02-07 11:46:45 -0500

    tests: test no args in data

--- Basalt.CommandParser.Tests/LoadingTests.cs
+++ Basalt.CommandParser.Tests/LoadingTests.cs
@@ -1,19 +1,42 @@
-﻿
+﻿using Basalt.CommandParser.Exceptions;
+
 namespace Basalt.CommandParser.Tests;
 
 [TestClass]
 public class LoadingTests
 {
+    private readonly string[] _args = Array.Empty<string>();
+    //private ProgramArguments _none;
+
+    //[TestInitialize]
+    //public void Setup_Arguments()
+    //{
+    //    _none = new NoArguments();
+    //}
+
+    private void ProcessArguments<TArgs>(string[] args) where TArgs : ProgramArguments, new()
+    {
+        var data = new TArgs();
+
+    }
+
+    private void ValidateFailure(ProgramArguments data, string[] args)
+    {
+        Assert.ThrowsException<ArgumentLoadingException>(() => data.Process(args));
+    }
+
     [TestMethod]
     public void Test_NoArgs()
     {
-        string[] args = { };
-
-        var data = new NoArguments();
-        data.Process(args);
-
-        //CommandParser.ProcessArguments<NoArguments>(args);
+        ValidateFailure(new NoArguments(), _args);
     }
+
+    //[DataTestMethod]
+    //[DataRow(_none, new string[] { })]
+    //public void Test_LoadingArguments(ProgramArguments data, string[] args)
+    //{
+    //    Assert.ThrowsException<ArgumentLoadingException>(() => data.Process(args));
+    //}
 }
 
 public class NoArguments : ProgramArguments
