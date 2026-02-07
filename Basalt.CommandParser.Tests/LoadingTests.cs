ff1fbed694e264ef03b2b0b6345d6560a80b7009
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-02-07 11:48:39 -0500

    tests: remove old tests

--- Basalt.CommandParser.Tests/LoadingTests.cs
+++ Basalt.CommandParser.Tests/LoadingTests.cs
@@ -1,4 +1,5 @@
-﻿using Basalt.CommandParser.Exceptions;
+﻿using Basalt.CommandParser.Attributes;
+using Basalt.CommandParser.Exceptions;
 
 namespace Basalt.CommandParser.Tests;
 
@@ -6,19 +7,6 @@ namespace Basalt.CommandParser.Tests;
 public class LoadingTests
 {
     private readonly string[] _args = Array.Empty<string>();
-    //private ProgramArguments _none;
-
-    //[TestInitialize]
-    //public void Setup_Arguments()
-    //{
-    //    _none = new NoArguments();
-    //}
-
-    private void ProcessArguments<TArgs>(string[] args) where TArgs : ProgramArguments, new()
-    {
-        var data = new TArgs();
-
-    }
 
     private void ValidateFailure(ProgramArguments data, string[] args)
     {
@@ -30,13 +18,6 @@ public class LoadingTests
     {
         ValidateFailure(new NoArguments(), _args);
     }
-
-    //[DataTestMethod]
-    //[DataRow(_none, new string[] { })]
-    //public void Test_LoadingArguments(ProgramArguments data, string[] args)
-    //{
-    //    Assert.ThrowsException<ArgumentLoadingException>(() => data.Process(args));
-    //}
 }
 
 public class NoArguments : ProgramArguments
